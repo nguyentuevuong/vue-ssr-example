5285b6d6128db104bd83e5b8574cb57d82709202
Nguyễn Tuệ Vương <vungtroixa@gmail.com>
2020-04-03 00:04:03 +0700

    init first files

---
+++ README.md
@@ -0,0 +1,16 @@
+# vue-ssr-example
+
+> A Vue.js with ssr project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build for production with minification
+npm run build
+
+# ssr render
+node ./server.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+var path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+const VueCSRPlugins = require('vue-server-renderer/client-plugin');
+
+module.exports = {
+  entry: './src/main.client.js',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'eval-source-map',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'build.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new VueCSRPlugins(),
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ]
+}
